c2ea9aedc8bdecaf456dc986789be6c228042154
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-07 23:09:28 +0000

    adds a function to generate a string of six random letters

--- express_server.js
+++ express_server.js
@@ -6,6 +6,16 @@ var PORT = process.env.PORT || 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  const possibleChoices = "abcdefghijklmnopqrstuvwxyz";
+  let randomString = "";
+
+  for(var i = 0; i <= 5; i++) {
+    randomString += possibleChoices.charAt(Math.floor(Math.random() * possibleChoices.length));
+  }
+  return randomString;
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,6 +34,13 @@ app.get("/urls/:id", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls_new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("OK");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
